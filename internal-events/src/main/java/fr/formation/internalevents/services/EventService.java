aefc7cc00af6e09d90399826593ea4ebfebe314a
Odylane <cnobyn@gmail.com>
2020-11-30 17:01:16 +0100

    update to retrieve a list of events

--- internal-events/src/main/java/fr/formation/internalevents/services/EventService.java
+++ internal-events/src/main/java/fr/formation/internalevents/services/EventService.java
@@ -1,9 +1,14 @@
 package fr.formation.internalevents.services;
 
+import java.util.List;
+
 import fr.formation.internalevents.dtos.EventCreateDto;
+import fr.formation.internalevents.dtos.EventShortInfoDto;
 
 public interface EventService {
-	
+
 	void create(EventCreateDto dto);
-	
+
+	List<EventShortInfoDto> getAll();
+
 }
