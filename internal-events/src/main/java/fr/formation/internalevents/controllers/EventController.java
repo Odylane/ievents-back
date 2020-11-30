45c15e61819d760e4a77db219e4c3055bace3531
Odylane <cnobyn@gmail.com>
2020-11-30 16:57:20 +0100

    update EventController to retrieve a list of events

--- internal-events/src/main/java/fr/formation/internalevents/controllers/EventController.java
+++ internal-events/src/main/java/fr/formation/internalevents/controllers/EventController.java
@@ -1,31 +1,39 @@
 package fr.formation.internalevents.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.formation.internalevents.dtos.EventCreateDto;
+import fr.formation.internalevents.dtos.EventShortInfoDto;
 import fr.formation.internalevents.services.EventService;
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
-	
+
 	private final EventService service;
-	
+
 	@Autowired
 	public EventController(EventService service) {
 		this.service = service;
 	}
-	
+
 	@PostMapping
 	public void createEvent(@Valid @RequestBody EventCreateDto dto) {
-		//dto.setEmployeeOrganizer(getEmployeeId());
 		service.create(dto);
+	}
+
+	@GetMapping
+	public List<EventShortInfoDto> getAll() {
+		return service.getAll();
 	}
 
 }
