4aa5dcc8fa8146fab9c5ed5110b0002f330059c1
Odylane <cnobyn@gmail.com>
2021-01-25 21:56:07 +0100

    derived query updated + events order by startDateTimeAsc

--- internal-events/src/main/java/fr/formation/internalevents/repositories/EventRepository.java
+++ internal-events/src/main/java/fr/formation/internalevents/repositories/EventRepository.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import fr.formation.internalevents.dtos.EventFullInfoDto;
 import fr.formation.internalevents.entities.Event;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-	List<Event> findByStartDateTimeGreaterThan(LocalDateTime startDateTime);
+	List<Event> findByStartDateTimeGreaterThanOrderByStartDateTimeAsc(LocalDateTime startDateTime);
+
+	EventFullInfoDto getById(Long id);
 
 }
